74d3a78f9b9aa1afc96a2a3a9dfe91aebaa3d841
sungmin <suilsungmin@naver.com>
2016-02-02 22:58:33 +0900

    Test

--- src/main/java/hello/web/MainController.groovy
+++ src/main/java/hello/web/MainController.groovy
@@ -15,27 +15,25 @@ class MainController {
     TodoRepository blogDao;
 
     @RequestMapping("/")
+    @ResponseBody
     def String index() {
-        return "Greetings from Spring Boot!"
+        "Greetings from Spring Boot!"
     }
 
     @RequestMapping(value="/{id}", method= RequestMethod.POST)
     public String insertBlog(TodoRepository todo) {
-        blogDao.save(todo);
-        return "Greetings from Spring Boot!";
+        //blogDao.save(todo);
     }
 
     @RequestMapping(value="/find", method= RequestMethod.GET)
     @ResponseBody
     def String getBlogs() {
         blogDao.findAll();
-        return "Greetings from Spring Boot!";
     }
 
     @RequestMapping(value="/find/{id}", method= RequestMethod.GET)
     @ResponseBody
     def String getBlog(TodoRepository todo) {
         blogDao.findOne(todo.getId());
-        return "Greetings from Spring Boot!";
     }
 }
